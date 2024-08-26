546a1a5ee04bd57292f8ab7e6dc4e73ed3bc65a7
Aiarashi0616 <morningaaa0616@gmail.com>
2024-08-26 23:44:57 +0900

    “19章の課題を提出する”

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Posts;
 
 class PostController extends Controller
 {
@@ -11,7 +12,14 @@ class PostController extends Controller
 
         // テーブルからすべてのデータを取得し、変数に代入する
         $posts = DB::table('posts')->get();
-        $name= "mizuno";
-        return view('posts/index',compact('posts','name'));
+        return view('posts.index',compact('posts'));
 }
+        public function show($id) {
+    // $idに基づいてデータベースから商品を取得
+    $post = Posts::find($id);
+    
+    // 取得した商品データをビューに渡して表示
+    return view('posts.show', compact('post'));
+}
+
 }
